c5538124c801ddc01371363856188f57c2be4d85
izzaaarhm <izzaarhm@gmail.com>
2024-09-27 21:29:05 +0700

    copy-an file bank_account.js ch1 & fitur deposit

--- bank_account.js
+++ bank_account.js
@@ -1,39 +1,16 @@
+class BankAccount {
+    constructor(owner, saldo) {
+        this.owner = owner;
+        this.saldo = saldo;
+    }
 
-let saldo = 0;
-
-function tambahSaldo() {
-   
-    let jumlah = parseFloat(window.prompt('Masukkan jumlah saldo yang ingin ditambahkan:'));
-
-    saldo += jumlah;
-    alert(`Saldo berhasil ditambahkan. Saldo akhir: Rp${saldo}`);
-}
-
-function kurangiSaldo() {
-    
-    let jumlah = parseFloat(window.prompt('Masukkan jumlah saldo yang ingin dikurangi:'));
-
-    if (jumlah <= saldo) {
-        saldo -= jumlah;
-        alert(`Saldo berhasil dikurangi. Saldo akhir: Rp${saldo}`);
-    } else {
-        alert('Saldo tidak cukup! :(');
+    deposit(amount) {
+        if (amount > 0) {
+            this.saldo += amount;
+            return this.saldo;
+        }else {
+            throw new Error("Nominal yang dimasukkan harus positif dan lebih dari 0!");
+        }
+        
     }
 }
-
-function pilihMenu() {
-    let pilihan = parseFloat(window.prompt('Masukkan pilihan menu:'))
-    
-    if (pilihan == 1)
-        alert(`Saldo Anda saat ini: Rp${saldo}`);
-    
-    else if (pilihan == 2) 
-        tambahSaldo()
-        
-    else if (pilihan == 3)
-        kurangiSaldo()
-    else 
-        alert('Input tidak valid');
-}
-
-pilihMenu()
